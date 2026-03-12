e0535992ce82a00998b7f3df481a570861fc57bc
ARAVINDH-1505 <srinivasaaravindh15@gmail.com>
2026-03-12 13:42:17 +0530

    this reranker folder that has the power to reoreder the highest matching chunk again by like 2 step verification

---
+++ app/reranker/cross_encoder.py
@@ -0,0 +1,18 @@
+from sentence_transformers import CrossEncoder
+
+model = CrossEncoder(
+    "cross-encoder/ms-marco-MiniLM-L-6-v2"
+)
+
+def rerank(query, documents):
+
+    pairs = [(query, doc.page_content) for doc in documents]
+
+    scores = model.predict(pairs)
+
+    ranked = sorted(
+        zip(scores, documents),
+        reverse=True
+    )
+
+    return [doc for score, doc in ranked[:5]]
